3f78217dc50baad5507ca57d0806acf3b3a70d2d
tak.song-at-585251656769 <>
2023-08-21 16:24:12 +0900

    Updates

--- src/section/index.js
+++
@@ -1,7 +0,0 @@
-import Main from "./Main";
-import Section1 from "./Section1";
-import Section2 from "./Section2";
-import Section3 from "./Section3";
-import Result from "./Result";
-
-export { Main, Section1, Section2, Section3, Result };
